215119465d5b21f5dab8ad9d341543b832a31749
Joe Kim <joesm.genie@gmail.com>
2023-11-24 16:27:58 +0700

    Move mongo connection to new file

--- server/src/databases/index.ts
+++ server/src/databases/index.ts
@@ -1,10 +1,1 @@
-import mongoose from 'mongoose';
-import { MONGO_DB, MONGO_HOST, MONGO_PORT, MONGO_USER, MONGO_PASS } from '../configs';
-
-const mongoClient = mongoose.connect(`mongodb://${MONGO_HOST}:${MONGO_PORT}`, {
-  user: MONGO_USER,
-  pass: MONGO_PASS,
-  dbName: MONGO_DB
-});
-
-export default mongoClient;
+export * from './mongo';
---
+++ server/src/databases/mongo.ts
@@ -0,0 +1,10 @@
+import mongoose from 'mongoose';
+import { MONGO_DB, MONGO_HOST, MONGO_PORT, MONGO_USER, MONGO_PASS } from '../configs';
+
+const mongoClient = mongoose.connect(`mongodb://${MONGO_HOST}:${MONGO_PORT}`, {
+  user: MONGO_USER,
+  pass: MONGO_PASS,
+  dbName: MONGO_DB
+});
+
+export default mongoClient;
