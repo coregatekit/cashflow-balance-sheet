e1ee0438fc5b8daf3013b6dd03ac0fada825d570
Joe Kim <joesm.genie@gmail.com>
2023-11-24 16:45:39 +0700

    Add redis config

--- server/src/configs/index.ts
+++ server/src/configs/index.ts
@@ -3,11 +3,17 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 const ENV = process.env.NODE_ENV || 'development';
-const MONGO_USER = process.env.MONGO_USER;
+
+const MONGO_USER = process.env.MONGO_USER || 'antman';
 const MONGO_PASS = process.env.MONGO_PASS || 'sup3r_secret';
 const MONGO_HOST = process.env.MONGO_HOST || 'localhost';
 const MONGO_PORT = process.env.MONGO_PORT || 27017;
 const MONGO_DB = process.env.MONGO_DB || 'cashflow';
+
+const REDIS_HOST = process.env.REDIS_HOST || 'localhost';
+const REDIS_PORT = process.env.REDIS_PORT || 6379;
+const REDIS_USER = process.env.REDIS_USER || 'default';
+const REDIS_PASS = process.env.REDIS_PASS || 'f*cking_s3cure_redis';
 
 export {
   ENV,
@@ -16,4 +22,8 @@ export {
   MONGO_HOST,
   MONGO_PORT,
   MONGO_DB,
+  REDIS_HOST,
+  REDIS_PORT,
+  REDIS_USER,
+  REDIS_PASS,
 }
