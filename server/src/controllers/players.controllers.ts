c3d276ffac6b33ce03155deb8088ab15765d7ff8
Joe Kim <joesm.genie@gmail.com>
2023-11-25 14:43:12 +0700

    Implement create player controller

---
+++ server/src/controllers/players.controllers.ts
@@ -0,0 +1,49 @@
+import { Request, Response } from 'express';
+import Player from '../models/player.model';
+import Session from '../models/session.model';
+import Profession from '../models/profession.model';
+
+type createPlayerDto = {
+  current_sessions: string;
+  name: string;
+  dreams: string;
+  choose_profession: string;
+};
+
+async function createPlayer(req: Request, res: Response) {
+  const { current_sessions, name, dreams, choose_profession }: createPlayerDto = req.body;
+
+  const session = await Session.findOne({ current_sessions });
+
+  if (session) {
+    const profession = await Profession.findOne({ profession: choose_profession });
+    if (profession) {
+      const player = await Player.create({
+        name,
+        dreams,
+        profession
+      });
+      player.total_incomes = profession.income.salary;
+      player.total_expenses = profession.expenses.reduce((prev, expense) => prev + expense.amount, 0);
+      player.cashflow_per_month = player.total_incomes - player.total_expenses;
+      player.save();
+
+      session.players.push({
+        playerId: player.id,
+        name: player.name,
+        cashflow: player.cashflow_per_month,
+      });
+      session.totalPlayer = session.players.length;
+      session.save();
+
+      return res.status(201).json(player);
+    }
+    return res.status(400).json({ msg: 'profession not found' });
+  } else {
+    return res.status(400).json({ msg: 'session not found!' });
+  }
+}
+
+export {
+  createPlayer,
+};
