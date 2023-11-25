426a5e80c9aefb9bebf0f3150124a4ffa89c2f98
Joe Kim <joesm.genie@gmail.com>
2023-11-25 14:42:40 +0700

    Update player schema

--- server/src/models/player.model.ts
+++ server/src/models/player.model.ts
@@ -5,18 +5,20 @@ export type PlayerType = {
   dreams: string;
   prefession: Types.ObjectId;
   passive_income: number;
+  total_children: number;
   total_incomes: number;
   total_expenses: number;
-  cashflow: number;
+  cashflow_per_month: number;
 };
 
 const playerSchema = new Schema<PlayerType>({
   name: { type: String, required: true },
   dreams: { type: String, required: true },
   prefession: { type: Schema.Types.ObjectId, ref: 'professions' },
+  total_children: { type: Number, default: 0 },
   passive_income: { type: Number, default: 0 },
   total_incomes: { type: Number, default: 0 },
-  cashflow: { type: Number, default: 0 },
+  cashflow_per_month: { type: Number, default: 0 },
 });
 
 const Player = model<PlayerType>('players', playerSchema);
