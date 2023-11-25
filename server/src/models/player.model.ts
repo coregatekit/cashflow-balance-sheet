4bbe011f2b0cda05c499d4ebdcc4045ac030db6a
Joe Kim <joesm.genie@gmail.com>
2023-11-25 13:31:25 +0700

    Add model player

---
+++ server/src/models/player.model.ts
@@ -0,0 +1,24 @@
+import { Schema, Types, model } from 'mongoose';
+
+type PlayerType = {
+  name: string;
+  dreams: string;
+  prefession: Types.ObjectId;
+  passive_income: number;
+  total_incomes: number;
+  total_expenses: number;
+  cashflow: number;
+}
+
+const playerSchema = new Schema<PlayerType>({
+  name: { type: String, required: true },
+  dreams: { type: String, required: true },
+  prefession: { type: Schema.Types.ObjectId, ref: 'professions' },
+  passive_income: { type: Number, default: 0 },
+  total_incomes: { type: Number, default: 0 },
+  cashflow: { type: Number, default: 0 },
+});
+
+const Player = model<PlayerType>('players', playerSchema);
+
+export default Player;
