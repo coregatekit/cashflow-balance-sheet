59f32d8ba99cd72f81924eda0d7ed943ad5e2929
Joe Kim <joesm.genie@gmail.com>
2023-11-25 13:10:07 +0700

    Add graceful shutdown function

--- server/src/index.ts
+++ server/src/index.ts
@@ -2,6 +2,8 @@ import dotenv from 'dotenv';
 
 import { createServer } from 'http';
 import app from './app';
+import redisClient from './databases/redis';
+import mongoClient from './databases/mongo';
 
 dotenv.config();
 
@@ -9,4 +11,33 @@ const PORT = process.env.PORT || 8000;
 const httpServer = createServer(app);
 httpServer.listen(PORT, () => {
   console.log(`Server is starting and running on http://localhost:${PORT}`);
-})
+});
+
+const gracefulShutdown = () => {
+  console.info('Get SIGTERM. Graceful shutdown start', new Date().toISOString());
+  try {
+    httpServer.close(async () => {
+      console.log('Closed out remaining connections.');
+      redisClient.disconnect();
+      (await mongoClient).connection.close();
+    });
+
+    setTimeout(async () => {
+      console.error('Could not close connections in time, forcefully shutting down');
+      redisClient.disconnect();
+      (await mongoClient).connection.close();
+    }, 10 * 1000);
+
+    console.log('All database disconnected.');
+  } catch (error) {
+    console.error(error);
+  } finally {
+    process.exit();
+  }
+}
+
+// listen for TERM signal .e.g kill
+process.on('SIGTERM', gracefulShutdown);
+
+// listen for INT signal e.c. Ctrl-C
+process.on('SIGINT', gracefulShutdown);
